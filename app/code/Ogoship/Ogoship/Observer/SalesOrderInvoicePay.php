41ec991c9cc565c75b780a0b485467678ddd05db
Uday Khuman <uday.khuman@sparsh1.com>
2018-05-22 11:28:04 +0530

    created function for sending the order to ogoship after successfull payment

---
+++ app/code/Ogoship/Ogoship/Observer/SalesOrderInvoicePay.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Ogoship\Ogoship\Observer;
+
+use Magento\Framework\Event\Observer as EventObserver;
+use Magento\Framework\Event\ObserverInterface;
+
+class SalesOrderInvoicePay implements ObserverInterface {
+
+    /**
+     * @param EventObserver $observer
+     * @return $this
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     */
+    protected $_redirect;
+    protected $_url;
+    private $_send;
+
+    public function __construct(
+    \Magento\Framework\App\Action\Context $context, \Magento\Framework\App\Response\Http $redirect, \Magento\Framework\UrlInterface $url,
+             \Ogoship\Ogoship\Controller\Adminhtml\Ogoship\Sendnettivarasto $send
+    ) {
+        parent::__construct($context);
+        $this->_send = $send;
+        $this->_redirect = $redirect;
+        $this->_url = $url;
+    }
+
+    public function execute(EventObserver $observer) {
+        $event = $observer->getEvent();
+//        $CustomRedirectionUrl = $this->_url->getUrl('ogoship/ogoship/sendnettivarasto');
+//        $this->_redirect->setRedirect($CustomRedirectionUrl);
+        $this->_send->execute();
+    }
+
+}
