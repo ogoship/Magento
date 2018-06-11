5f02fe560ccb9bedc704713050997fa131739a44
iloppi <loppi@iki.fi>
2018-06-11 09:52:32 +0300

    Fix latest changes getting called twice.

--- app/code/Ogoship/Ogoship/Cron/ProductOrderUpdate.php
+++ app/code/Ogoship/Ogoship/Cron/ProductOrderUpdate.php
@@ -4,7 +4,7 @@ class ProductOrderUpdate {
     public function execute() {
     	try {
 			$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-			$response = $objectManager->get('\Ogoship\Ogoship\Model\Ogoship')->get_latest_changes();
+			//$response = $objectManager->get('\Ogoship\Ogoship\Model\Ogoship')->get_latest_changes();
 			$deny_latest_changes = $objectManager->get('\Ogoship\Ogoship\Model\Ogoship')->getCurrentStoreConfigValue('Ogoship/view/deny_latest_changes');
 			if(!empty($deny_latest_changes)) {
 				$this->messageManager->addError(__('Last changes has been denied'));
